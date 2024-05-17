4e0c0f886df9ac7201fd384558430c7af18e1889
dmndpl <diamandopol.andrei10@gmail.com>
2024-05-17 17:40:44 +0100

    Create can create, currently overrides. Need to add append behaviour

--- src/commands/create.rs
+++ src/commands/create.rs
@@ -3,9 +3,10 @@ use crate::state::habit_metadata;
 
 pub fn run(name: String, learn: Mode, threshold: u32, frequency: Frequency) -> Result<(), Box<dyn std::error::Error>> {
     let mut habits = habit_metadata::get_or_create()?;
+    let prev_key = 2;
 
-    habits.insert(1, Habit {
-        id: 1,
+    habits.insert(prev_key + 1, Habit {
+        id: prev_key + 1,
         name: name,
         learn: learn,
         threshold: threshold,

6d48358dc776a1c450d00ae36a2d8bd5e64fcdd3
dmndpl <diamandopol.andrei10@gmail.com>
2024-05-17 17:47:43 +0100

    Add append behavior to create

--- src/commands/create.rs
+++ src/commands/create.rs
@@ -3,10 +3,11 @@ use crate::state::habit_metadata;
 
 pub fn run(name: String, learn: Mode, threshold: u32, frequency: Frequency) -> Result<(), Box<dyn std::error::Error>> {
     let mut habits = habit_metadata::get_or_create()?;
-    let prev_key = 2;
+    let prev_key = habits.keys().max().unwrap_or_else(|| {&0});
+    let new_key = prev_key + 1;
 
-    habits.insert(prev_key + 1, Habit {
-        id: prev_key + 1,
+    habits.insert(new_key, Habit {
+        id: new_key,
         name: name,
         learn: learn,
         threshold: threshold,
